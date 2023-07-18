6a19cd5997172528103b72772eea013ebbc5375f
zc <1972862096@qq.com>
2023-07-18 17:22:03 +0800

    Merge branch 'main' of https://github.com/2023zc/study-javaSE
                 # Conflicts:
                 #	.idea/modules.xml
                 补充圆的有参构造

--- practice_javaSE/src/Point_Circle/Circle.java
+++ practice_javaSE/src/Point_Circle/Circle.java
@@ -2,27 +2,31 @@ package Point_Circle;
 
 public class Circle {
     private Point Center;
-    private double r;
+    private double R;
     Circle(){   //初始化值
         this.Center=new Point();
-        this.Center.setX(0);
-        this.Center.setY(0);
     }
-    public boolean check(double r){
+
+    public Circle(Point center, double r) {
+        Center = center;
+        R = r;
+    }
+
+    private boolean check(double r){
         return r>0;
     }
     public double getR(){    //返回半径
-        if(check(this.r)){
-            return r;
+        if(check(R)){
+            return R;
         }
         else{
             System.out.println("半径设置有误");
         }
         return 0;
     }
-    public Point getCenter(){   //放回圆的圆心
+    public Point getCenter(){   //返回圆的圆心
         Point new_point=new Point();
-        if(check(this.r)) {
+        if(check(R)) {
             return this.Center;
         }
         else{
@@ -31,8 +35,8 @@ public class Circle {
         return new_point;
     }
     public double getArea(){   //返回圆的面积
-        if(check(this.r)){
-            return Math.PI*r*r;
+        if(check(R)){
+            return Math.PI*R*R;
         }
         return 0;
     }
@@ -41,7 +45,7 @@ public class Circle {
             System.out.println("半径设置有误!");
             return;
         }
-        this.r=r;
+        R=r;
     }
     public void setCenter(Point center){     //设置圆心
         this.Center.setX(center.getX());
@@ -49,7 +53,7 @@ public class Circle {
     }
     public Circle My_clone(){   //相当于深拷贝,新开辟了一个内存空间
         Circle new_circle=new Circle();
-        if(check(this.r)){
+        if(check(R)){
             new_circle.setCenter(this.getCenter());
             new_circle.setR(this.getR());
             return new_circle;
