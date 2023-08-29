6185379ebef7d2a351eb1073d0cb6507d50d543d
zc <1972862096@qq.com>
2023-08-29 22:36:04 +0800

    leetcode刷题:https://leetcode-cn.com/problems/path-sum-ii/ 二叉树中和为某一值的路径 添加了注解

--- practice_javaSE/src/leetcode/PathSum.java
+++ practice_javaSE/src/leetcode/PathSum.java
@@ -16,26 +16,27 @@ public class PathSum {
         Target=target;
         if(root==null) return ans;
         sum+=root.val;
-        List<Integer> pieces=new ArrayList<>();
+        List<Integer> pieces=new ArrayList<>();  //初始化
         findans(sum,root,pieces);
         return ans;
     }
 
     private void findans(int sum, TreeNode nownode,List<Integer> pieces) {
         pieces.add(nownode.val);
-        if(nownode.left==null&&nownode.right==null){
+        if(nownode.left==null&&nownode.right==null){  //终止条件
             if(sum==Target) {
-                ans.add(new ArrayList<>(pieces));
+                ans.add(new ArrayList<>(pieces));  //注意要新建一个List,因为这里的添加都是地址传递,如果直接添加的话后面在修改pieces时也会同时修改ans
             }
+            //记得回溯
             return;
         }
         if(nownode.left!=null) {
             findans(sum+nownode.left.val,nownode.left,pieces);
-            pieces.remove(pieces.size()-1);
+            pieces.remove(pieces.size()-1);  //每次回溯都要将最后一个数删除
         }
         if(nownode.right!=null){
             findans(sum+nownode.right.val,nownode.right,pieces);
-            pieces.remove(pieces.size()-1);
+            pieces.remove(pieces.size()-1);  //每次回溯都要将最后一个数删除
         }
 
     }
